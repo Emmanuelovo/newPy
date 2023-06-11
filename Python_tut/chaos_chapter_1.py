89c2659cd76bce73be8fac7607042ec9dcbd74ac
Emmanuelovo <113689114+Emmanuelovo@users.noreply.github.com>
2023-06-11 16:18:14 +0100

    My chapter 1 and 2 work

---
+++ Python_tut/chaos_chapter_1.py
@@ -0,0 +1,18 @@
+# chaos.py
+# This is a function to illustrate the behaviours of random numbers
+def main():
+    print("This is a program to illustrate the chaotic behaviour of numbers")
+
+    #input session
+    x = eval(input("Enter a number from 0 to 1: "))
+    y = eval(input("Enter a number from 0 to 1: "))
+
+    n = eval(input("How many values do want me to print?:"))
+
+    #process session
+    for i in range(n):
+        x = 2.0 * x *(1 - x)
+        y = 2.0 * x *(1 - x)
+
+        #output session
+        print(x,              y)
